dc86655ea78deea9ff013558b4d2cf7b0f59d221
helmesjo <helmesjo@live.com>
2015-11-22 03:39:46 +0100

    Setting up project with test-project. Added NUnit & NSubstitute, but tests are not showing up in explorer. I really fucking hate bugs.

---
+++ Project/GOAP.Tests.Unit/Planner/PlannerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using NUnit.Framework;
+using NSubstitute;
+
+namespace GOAP.Tests.Unit.Planner
+{
+	[TestFixture]
+	public class PlannerTests
+	{
+
+		[Test]
+		public void FindPlan_PassValidStartAndEndNodes_ReturnsThePlan()
+		{
+			Assert.IsTrue(false);
+		}
+
+	}
+}
